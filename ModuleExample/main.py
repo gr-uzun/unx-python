9948128d4b7b9efa2c0a0364d79fdec39a364a89
Gürkan UZUN <gr.uzun@gmail.com>
2021-04-10 20:39:36 +0300

    modules and packages added.

---
+++ ModuleExample/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+
+ones = [1 for i in range(5)]
+
+print(suml(zeroes))
+
+print(prodl(ones))
+
